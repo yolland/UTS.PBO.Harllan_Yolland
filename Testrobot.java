a85eab5c5bafd7f3aec70df5a16e65a016c476ec
Yolland <arlanharland@gmail.com>
2015-05-12 17:30:58 +0700

    Revert "ini uts saya pak"
    This reverts commit 4ed55c486dc7c655b57442f0b0c94d4125b8fd24.

--- Testrobot.java
+++
@@ -1,9 +0,0 @@
-public class Testrobot {
-    public static void main(String[] args)
-    {
-        Asimo13 unit = new Asimo13(30, 150, 1000, 5);
-        unit.CetakSpesifikasi();
-        unit.Upgrade(20);
-        unit.Upgrade(25);
-    }
-}
